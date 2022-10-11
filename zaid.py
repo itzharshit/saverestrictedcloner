e9b5141aece383b7f040f611588aed2d2a188384
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2022-10-11 15:05:29 +0530

    Update zaid.py

--- zaid.py
+++ zaid.py
@@ -36,7 +36,7 @@ async def clone(bot: ZAID, msg: Message):
         await client.start()
         idle()
         user = await client.get_me()
-        await text.edit(f"Successfully started your bot.",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Start Bot", url="fhttps://t.me/{user.username}")]]))
+        await text.edit(f"Successfully started your bot.",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Start Bot", url=f"https://t.me/{user.username}")]]))
     except Exception as e:
         print(e) 
         await text.edit(f"An error occurred, please check your BOT_TOKEN")
